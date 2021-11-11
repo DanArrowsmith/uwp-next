f7eef0c673ac389dfe0f1ff4119c2ea11e300e87
DanArrowsmith <45849271+DanArrowsmith@users.noreply.github.com>
2021-11-11 05:28:15 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     NETLIFY_URL:
       process.env.NODE_ENV !== 'production'
         ? 'http://localhost:8888'
-        : 'https://xenodochial-hugle-1c192a.netlify.app/',
+        : 'https://xenodochial-hugle-1c192a.netlify.app',
   },
 };
